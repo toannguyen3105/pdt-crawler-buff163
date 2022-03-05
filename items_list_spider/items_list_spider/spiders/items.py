19e9d78e86b1ce05ed059b866d50fde7fcd7573c
tony <nguyenhuytoan1994@outlook.com>
2022-03-05 11:25:28 +0700

    Update item in page 2..

--- items_list_spider/items_list_spider/spiders/items.py
+++ items_list_spider/items_list_spider/spiders/items.py
@@ -3,8 +3,8 @@ from scrapy.http import Request
 from decouple import config
 
 COOKIES = config('COOKIE')
-CSGO_URL = "https://buff.163.com/api/market/goods?game=csgo&page_num=1&min_price=50&max_price=30000"
-DOTA_URL = "https://buff.163.com/api/market/goods?game=dota2&page_num=1"
+CSGO_URL = "https://buff.163.com/api/market/goods?game=csgo&page_num=1&min_price=50&max_price=30000&sort_by=price.desc"
+DOTA_URL = "https://buff.163.com/api/market/goods?game=dota2&page_num=1&sort_by=price.desc"
 CSGO_STR = "csgo"
 DOTA_STR = "dota2"
 # Set the headers here.
@@ -14,7 +14,10 @@ HEADERS = {
 
 
 def generate_url(gameId, page):
-    return 'goods?game={}&page_num={}&page_size=32'.format(CSGO_STR if gameId == 730 else DOTA_STR, page + 1)
+    if gameId == 730:
+        return 'goods?game={}&page_num={}&min_price=50&max_price=30000&sort_by=price.desc'.format(CSGO_STR, page + 1)
+    else:
+        return 'goods?game={}&page_num={}&sort_by=price.desc'.format(DOTA_STR, page + 1)
 
 
 class ItemsSpider(scrapy.Spider):
